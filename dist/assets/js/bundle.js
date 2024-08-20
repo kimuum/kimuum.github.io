64dbd302e753843d5aeff207ea0648f94ff8c1cf
yunikim <87588770+kimuum@users.noreply.github.com>
2024-08-20 11:00:09 +0900

    main 작업중

--- dist/assets/js/bundle.js
+++ dist/assets/js/bundle.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-  gsap.registerPlugin();
-  const heroBox = document.querySelectorAll('.hero-box');
+  gsap.registerPlugin(ScrollTrigger);
+  moveHero();
+  moveVisual();
+  moveWork();
 
   /* lenis 스크롤 효과 */
   const lenis = new Lenis();
@@ -16,26 +18,76 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   gsap.ticker.lagSmoothing(0);
+});
 
-  /* gsap */
+/* HERO */
+function moveHero() {
+  const heroBox = document.querySelectorAll('.hero-box');
+  heroBox.forEach((item) => {
+    gsap.to(item, {
+      y: '100%',
+      ease: 'power1.inOut',
+      scrollTrigger: {
+        trigger: item,
+        start: () => 'top top+=' + item.getBoundingClientRect().top,
+        end: 'top top',
+        scrub: !0,
+        // markers: true,
+      },
+    });
+  });
+
   gsap.to('.hero-word', {
     opacity: 1,
     delay: 1,
     ease: 'power4.inOut',
     stagger: 0.16,
   });
-  heroBox.forEach((el) => {
-    gsap.to(el, {
-      y: '100%',
-      ease: 'power1.inOut',
+}
+/* WORK */
+function moveWork() {
+  const worskBox = document.querySelectorAll('.work-box svg:not(:first-of-type)');
+  worskBox.forEach(function (item, i) {
+    var layer = item.getBoundingClientRect();
+    gsap.to(item, {
+      x: function x() {
+        return -(layer.width * i);
+      },
+      opacity: '0.5',
+      ease: 'none',
       scrollTrigger: {
-        trigger: el,
-        start: () => 'top top+=' + el.getBoundingClientRect().top,
-        end: 'top top',
-        scrub: !0,
+        trigger: item,
+        start: 'top center',
+        end: '+=300%',
+        scrub: true,
         markers: true,
       },
     });
-    console.log(el.getBoundingClientRect().top);
   });
-});
+
+  const swiperWork = new Swiper('.work-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    loop: false,
+    allowTouchMove: true,
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+}
+/* VISUAL */
+function moveVisual() {
+  const visualWord = document.querySelectorAll('.visual-word');
+  visualWord.forEach(function (item) {
+    gsap.to(item, {
+      opacity: '1',
+      scrollTrigger: {
+        trigger: item,
+        start: '-=400%',
+        end: 'bottom center',
+        scrub: true,
+        // markers: true,
+      },
+    });
+  });
+}
