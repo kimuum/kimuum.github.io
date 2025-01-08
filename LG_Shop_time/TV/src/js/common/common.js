42e03d2a7b82943812974952e191b1b6154db575
김윤희 <HL0793@hivelab.co.kr>
2025-01-08 15:16:04 +0900

    기존삭제

--- LG_Shop_time/TV/src/js/common/common.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const init = document.querySelector('html');
-  const wrapper = init.querySelector('.wrapper');
-  init.classList.add('scroll');
-  wrapper.style.cssText = 'min-height: 1080px';
-});
--- lg_insight_plus/src/js/_datepicker.js
+++
@@ -1,6 +0,0 @@
-function dateRangePicker(date) {
-  const elem = document.getElementById(date);
-  const dateRangePicker = new DateRangePicker(elem, {
-    language: 'ko',
-  });
-}
